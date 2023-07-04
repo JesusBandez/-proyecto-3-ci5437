2527a6b8c5ce3f12b3281ed42535b28637628dce
Jesus_B <17-10046@usb.ve>
2023-07-04 12:06:36 -0400

    init parser

---
+++ Solver/variables/Parser.py
@@ -0,0 +1,11 @@
+class Parser():
+    def __init__(self, vars):
+        self.vars = vars
+
+class Asignation():
+    def __init__(self, local, away, day, slot):
+        self.local = local
+        self.away = away
+        self.day = day
+        self.slot = slot
+        
