27195a186636e32be04000c0bcf7ffc572b8df52
Jesus_B <17-10046@usb.ve>
2023-07-11 18:27:53 -0400

    Removed benchmar outputs and update .gitignore

--- Solver/benchmarks/benchmark.sh
+++ Solver/benchmarks/benchmark.sh
@@ -1,6 +1,6 @@
 cd ../
 
-for i in {0..8}
+for i in {8..8}
     do
     echo "Ejecutando archivo ${i}.json" >> ./benchmarks/output.txt
     (time python3 main.py benchmarks/${i}.json) >> ./benchmarks/output.txt 2>&1
